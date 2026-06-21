52565051565744ac82614df2d707231908de5add
Faruk Dev <farukmia7979@gmail.com>
2026-06-21 21:37:37 +0600

    fixed web.php syntax error

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,19 @@ use Illuminate\Support\Facades\Artisan;
 
 Route::get('/migrate', function () {
     try {
+      
+        Artisan::call('route:clear');
+        Artisan::call('config:clear');
+        Artisan::call('cache:clear');
+        
         Artisan::call('migrate', ['--force' => true]);
-        return "Migration successful! <br><br> Output: " . Artisan::output();
+        
+        return "Everything is cleared and Migration is successful! <br><br> Output: " . Artisan::output();
     } catch (\Exception $e) {
         return "Error: " . $e->getMessage();
     }
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => view('welcome'));
 
-Route::get('{any}', function () {
-    return view('welcome');
-})->where('any', '.*');
+Route::get('{any}', fn() => view('welcome'))->where('any', '.*');
