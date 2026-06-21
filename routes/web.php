410ca4b4d5e62666ddfa5520d746148274d848f7
Faruk Dev <farukmia7979@gmail.com>
2026-06-21 22:01:29 +0600

    removed migrate route from web.php

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
-Route::get('/migrate', function () {
-    try {
-      
-        Artisan::call('route:clear');
-        Artisan::call('config:clear');
-        Artisan::call('cache:clear');
-        
-        Artisan::call('migrate', ['--force' => true]);
-        
-        return "Everything is cleared and Migration is successful! <br><br> Output: " . Artisan::output();
-    } catch (\Exception $e) {
-        return "Error: " . $e->getMessage();
-    }
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/', fn() => view('welcome'));
-
-Route::get('{any}', fn() => view('welcome'))->where('any', '.*');
+Route::get('{any}', function () {
+    return view('welcome'); 
+})->where('any', '.*');
