061891017a22bbab6e93ae744f841fac9593763d
Faruk Dev <farukmia7979@gmail.com>
2026-06-22 17:08:00 +0600

    added directory fix for vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+
             if (!is_dir(config('view.compiled'))) {
-                URL::forceScheme('https');
                 mkdir(config('view.compiled'), 0755, true);
             }
         }
