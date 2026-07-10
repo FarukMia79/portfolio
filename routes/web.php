07c3fb6f80515e2f446caaf75d84e4eec9f367ee
Faruk Dev <farukmia7979@gmail.com>
2026-07-10 21:58:35 +0600

    Added migration route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/run-migrate', function () {
+    Artisan::call('migrate --force');
+    return "Database migrated successfully!";
+});
 
 Route::get('/', function () {
     return view('welcome');
