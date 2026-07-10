f089df9676544151699d961d61895072ea6c806f
Faruk Dev <farukmia7979@gmail.com>
2026-07-10 23:31:35 +0600

    Added admin seeder and seed route

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,16 @@
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-
+/*
 Route::get('/run-migrate', function () {
     Artisan::call('migrate --force');
     return "Database migrated successfully!";
+});
+*/
+
+Route::get('/run-seed', function () {
+    Artisan::call('db:seed --force');
+    return "Admin Data Seeded Successfully!";
 });
 
 Route::get('/', function () {
