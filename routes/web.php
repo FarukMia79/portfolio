506d4a15982157da5345c8d8ac9971b32423de0e
Faruk Dev <farukmia7979@gmail.com>
2026-07-13 22:48:14 +0600

    remove manual sql route

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
 
-
+/*
 Route::get('/run-migrate', function () {
     Artisan::call('migrate --force');
     return "Database migrated successfully!";
@@ -15,17 +14,7 @@ Route::get('/run-seed', function () {
     return "Admin Data Seeded Successfully!";
 });
 
-Route::get('/run-sql', function () {
-    try {
-        DB::statement('ALTER TABLE projects ADD COLUMN github_link VARCHAR(255) NULL;');
-        DB::statement('ALTER TABLE projects ADD COLUMN live_demo_link VARCHAR(255) NULL;');
-        return "Columns added successfully via RAW SQL!";
-    } catch (\Exception $e) {
-        return "Error: " . $e->getMessage();
-    }
-});
-
-
+*/
 
 
 
